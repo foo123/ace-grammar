efe572e6eda2cde4517dc2d9065c7e4ef6c195b2
Nikos M <nikosms0@gmail.com>
2015-10-22 23:36:07 +0300

    2.5.0-pre updates, edits, format sample grammars

--- test/grammars/javascript.js
+++ test/grammars/javascript.js
@@ -1,176 +1,116 @@
-// 1. an almost complete javascript grammar in simple JSON format
+// 1. a partial javascript grammar in simple JSON format
 var js_grammar = {
         
-    // prefix ID for regular expressions used in the grammar
-    "RegExpID": "RegExp::",
+// prefix ID for regular expressions used in the grammar
+"RegExpID"                          : "RegExp::",
     
-    // Style model
-    "Style": {
-        // lang token type  -> Editor (style) tag
-        "comment":    "comment",
-        "atom":       "constant",
-        "keyword":    "keyword",
-        "this":       "keyword",
-        "builtin":    "support",
-        "operator":   "operator",
-        "identifier": "identifier",
-        "property":   "constant.support",
-        "number":     "constant.numeric",
-        "string":     "string",
-        "regex":      "string.regexp"
-    },
+// Style model
+"Style"                             : {
 
-    // Lexical model
-    "Lex": {
-        
-        // comments
-        "comment" : {
-            "type" : "comment",
-            "tokens" : [
-                // line comment
-                // start, end delims  (null matches end-of-line)
-                [  "//",  null ],
-                // block comments
-                // start,  end    delims
-                [  "/*",   "*/" ]
-            ]
-        },
-        
-        // general identifiers
-        "identifier" : "RegExp::/[_A-Za-z$][_A-Za-z0-9$]*/",
-        
-        "this" : "RegExp::/this\\b/",
-        
-        "property" : "RegExp::/[_A-Za-z$][_A-Za-z0-9$]*/",
-        
-        // numbers, in order of matching
-        "number" : [
-            // floats
-            "RegExp::/\\d*\\.\\d+(e[\\+\\-]?\\d+)?/",
-            "RegExp::/\\d+\\.\\d*/",
-            "RegExp::/\\.\\d+/",
-            // integers
-            // hex
-            "RegExp::/0x[0-9a-fA-F]+L?/",
-            // binary
-            "RegExp::/0b[01]+L?/",
-            // octal
-            "RegExp::/0o[0-7]+L?/",
-            // decimal
-            "RegExp::/[1-9]\\d*(e[\\+\\-]?\\d+)?L?/",
-            // just zero
-            "RegExp::/0(?![\\dx])/"
-        ],
+     "comment"                      : "comment"
+    ,"atom"                         : "constant"
+    ,"keyword"                      : "keyword"
+    ,"this"                         : "keyword"
+    ,"builtin"                      : "support"
+    ,"operator"                     : "operator"
+    ,"identifier"                   : "identifier"
+    ,"property"                     : "constant.support"
+    ,"number"                       : "constant.numeric"
+    ,"string"                       : "string"
+    ,"regex"                        : "string.regexp"
 
-        // usual strings
-        "string" : {
-            "type" : "escaped-block",
-            "escape" : "\\",
-            // start, end of string (can be the matched regex group ie. 1 )
-            "tokens" : [ "RegExp::/(['\"])/",   1 ]
-        },
-        
-        // literal regular expressions
-        "regex" : {
-            "type" : "escaped-block",
-            "escape" : "\\",
-            // javascript literal regular expressions can be parsed similar to strings
-            "tokens" : [ "/",    "RegExp::#/[gimy]{0,4}#" ]
-        },
-        
-        // operators
-        "operator" : {
-            "combine" : true,
-            "tokens" : [
-                "\\", "+", "-", "*", "/", "%", "&", "|", "^", "~", "<", ">" , "!",
-                "||", "&&", "==", "!=", "<=", ">=", "<>", ">>", "<<",
-                "===", "!==", "<<<", ">>>" 
-            ]
-        },
-        
-        // delimiters
-        "delimiter" : {
-            "combine" : true,
-            "tokens" : [
-                "(", ")", "[", "]", "{", "}", ",", "=", ";", "?", ":",
-                "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "++", "--",
-                ">>=", "<<="
-            ]
-        },
-            
-        // atoms
-        "atom" : {
-            // enable autocompletion for these tokens, with their associated token ID
-            "autocomplete" : true,
-            "tokens" : [
-                "true", "false", 
-                "null", "undefined", 
-                "NaN", "Infinity"
-            ]
-        },
+},
 
-        // keywords
-        "keyword" : {
-            // enable autocompletion for these tokens, with their associated token ID
-            "autocomplete" : true,
-            "tokens" : [ 
-                "if", "while", "with", "else", "do", "try", "finally",
-                "return", "break", "continue", "new", "delete", "throw",
-                "var", "const", "let", "function", "catch",
-                "for", "switch", "case", "default",
-                "in", "typeof", "instanceof"
-            ]
-        },
-        
-        // builtins
-        "builtin" : {
-            // enable autocompletion for these tokens, with their associated token ID
-            "autocomplete" : true,
-            "tokens" : [ 
-                "Object", "Array", "String", "Number", "RegExp", "Exception",
-                "setTimeout", "setInterval", "alert", "console"
-            ]
-        }
-    },
+// Lexical model
+"Lex"                               : {
     
-    // Syntax model (optional)
-    "Syntax": {
-        
-        "dotProperty" : {
-            "type" : "group",
-            "match" : "all",
-            "tokens" : [ ".", "property" ]
-        },
-        
-        "builtinOrIdentifier" : {
-            "type" : "group",
-            "match" : "either",
-            "tokens" : [ "}", ")", "this", "builtin", "identifier", "dotProperty" ]
-        },
-        
-        "dotProperties" : {
-            "type" : "group",
-            "match" : "zeroOrMore",
-            "tokens" : [ "dotProperty" ]
-        },
-        
-        "builtinOrIdentifierWithProperties" : {
-            "type" : "n-gram",
-            "tokens" : [
-                [ "builtinOrIdentifier", "dotProperties" ]
-            ]
-        }
-    },
+     "comment"                      : {"type":"comment","tokens":[
+                                    // line comment
+                                    // start, end delims  (null matches end-of-line)
+                                    [  "//",  null ],
+                                    // block comments
+                                    // start,  end    delims
+                                    [  "/*",   "*/" ]
+                                    ]}
+    ,"identifier"                   : "RE::/[_A-Za-z$][_A-Za-z0-9$]*/"
+    ,"this"                         : "RE::/this\\b/"
+    ,"property"                     : "RE::/[_A-Za-z$][_A-Za-z0-9$]*/"
+    ,"number"                       : [
+                                    // floats
+                                    "RE::/\\d*\\.\\d+(e[\\+\\-]?\\d+)?/",
+                                    "RE::/\\d+\\.\\d*/",
+                                    "RE::/\\.\\d+/",
+                                    // integers
+                                    // hex
+                                    "RE::/0x[0-9a-fA-F]+L?/",
+                                    // binary
+                                    "RE::/0b[01]+L?/",
+                                    // octal
+                                    "RE::/0o[0-7]+L?/",
+                                    // decimal
+                                    "RE::/[1-9]\\d*(e[\\+\\-]?\\d+)?L?/",
+                                    // just zero
+                                    "RE::/0(?![\\dx])/"
+                                    ]
+    ,"string"                       : {"type":"escaped-block","escape":"\\","tokens":
+                                    // start, end of string (can be the matched regex group ie. 1 )
+                                    [ "RE::/(['\"])/",   1 ]
+                                    }
+    ,"regex"                        : {"type":"escaped-block","escape":"\\","tokens":
+                                    // javascript literal regular expressions can be parsed similar to strings
+                                    [ "/",    "RE::#/[gimy]{0,4}#" ]
+                                    }
+    ,"operator"                     : {"tokens":[
+                                    "+", "-", "++", "--", "%", ">>", "<<", ">>>",
+                                    "*", "/", "^", "|", "&", "!", "~",
+                                    ">", "<", "<=", ">=", "!=", "!==",
+                                    "=", "==", "===", "+=", "-=", "%=",
+                                    ">>=", ">>>=", "<<=", "*=", "/=", "|=", "&="
+                                    ]}
+    ,"delimiter"                    : {"tokens":[
+                                    "(", ")", "[", "]", "{", "}", ",", "=", ";", "?", ":",
+                                    "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "++", "--",
+                                    ">>=", "<<="
+                                    ]}
+    ,"atom"                         : {"autocomplete":true,"tokens":[
+                                    "true", "false", 
+                                    "null", "undefined", 
+                                    "NaN", "Infinity"
+                                    ]}
+    ,"keyword"                      : {"autocomplete":true,"tokens":[ 
+                                    "if", "while", "with", "else", "do", "try", "finally",
+                                    "return", "break", "continue", "new", "delete", "throw",
+                                    "var", "const", "let", "function", "catch", "void",
+                                    "for", "switch", "case", "default", "class", "import", "yield",
+                                    "in", "typeof", "instanceof"
+                                    ]}
+    ,"builtin"                      : {"autocomplete":true,"tokens":[ 
+                                    "Object", "Function", "Array", "String", 
+                                    "Date", "Number", "RegExp", "Math", "Exception",
+                                    "setTimeout", "setInterval", "parseInt", "parseFloat", 
+                                    "isFinite", "isNan", "alert", "prompt", "console", 
+                                    "window", "global", "this"
+                                    ]}
 
-    // what to parse and in what order
-    "Parser": [
-        "comment",
-        "number",
-        "string",
-        "regex",
-        "keyword",
-        "operator",
-        "atom",
-        "builtinOrIdentifierWithProperties"
-    ]
+},
+
+// Syntax model (optional)
+"Syntax"                            : {
+    
+    "dotProperty"                   : {"sequence":[".", "property"]}
+
+},
+
+// what to parse and in what order
+"Parser"                            : [
+                                    "comment",
+                                    "number",
+                                    "string",
+                                    "regex",
+                                    "keyword",
+                                    "operator",
+                                    "atom",
+                                    [ "'}' | ')' | this | builtin | identifier | dotProperty", "dotProperty*" ]
+                                    ]
+
 };
